6ff3245fd3c209c5e6ccdb6b67c37373c5e9eef7
DJ <deepjyoti_rajkhowa@yahoo.com>
2024-03-23 13:34:46 -0400

    Completed homework for week 6 - streaming

---
+++ java/kafka_examples/src/main/java/org/example/Secrets.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Secrets {
+    public static final String KAFKA_CLUSTER_KEY = "KXXBSZZRNYLIJLHQ";
+    public static final String KAFKA_CLUSTER_SECRET = "Vp7wXyuROfYj2xHfTlYDsBJPZvIYUGXIDY6dvtU+1Rdowz8lAk0LS5nGPrYnz2qN";
+
+    public static final String SCHEMA_REGISTRY_KEY = "REPLACE_WITH_SCHEMA_REGISTRY_KEY";
+    public static final String SCHEMA_REGISTRY_SECRET = "REPLACE_WITH_SCHEMA_REGISTRY_SECRET";
+
+}
